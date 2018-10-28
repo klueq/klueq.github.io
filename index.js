f3a3f2a4d9705f4cb10c949b08ed37c81773a0d6
klueq <43359723+klueq@users.noreply.github.com>
2018-10-27 21:39:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 window.IPFS = window.IPFS || window.Ipfs;
-window.ipfs = new IPFS;
+window.ipfs = new IPFS({
+  config: {
+    Addresses: {
+      Swarm: [
+        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+      ]
+    }
+  },  
+  EXPERIMENTAL: {
+    pubsub: true
+  }
+});
 
 ipfs.on('ready', () => {
   console.log('window.ipfs is ready');
