e7e8462de7dd565a006a84cb203058c0ff644223
klueq <43359723+klueq@users.noreply.github.com>
2018-10-27 21:58:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-log('initializing ipfs');
+window.onload = () => {
+  log('initializing ipfs');
 
-window.IPFS = window.IPFS || window.Ipfs;
-window.ipfs = new IPFS({
-  config: {
-    Addresses: {
-      Swarm: [
-        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
-      ]
+  window.IPFS = window.IPFS || window.Ipfs;
+  window.ipfs = new IPFS({
+    config: {
+      Addresses: {
+        Swarm: [
+          '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+        ]
+      }
+    },  
+    EXPERIMENTAL: {
+      pubsub: true
     }
-  },  
-  EXPERIMENTAL: {
-    pubsub: true
-  }
-});
+  });
 
-const fcid = 'QmQ2r6iMNpky5f1m4cnm3Yqw8VSvjuKpTcK1X7dBR1LkJF';
+  const fcid = 'QmQ2r6iMNpky5f1m4cnm3Yqw8VSvjuKpTcK1X7dBR1LkJF';
 
-ipfs.on('ready', () => {
-  log('window.ipfs is ready');
-  log('ipfs.files.get ' + fcid);
-  
-  ipfs.files.get(fcid).then(
-    res => log('ipfs.files.get -> ' + res),
-    err => log('ipfs.files.get -> ' + err));
-});
+  ipfs.on('ready', () => {
+    log('window.ipfs is ready');
+    log('ipfs.files.get ' + fcid);
+
+    ipfs.files.get(fcid).then(
+      res => log('ipfs.files.get -> ' + res),
+      err => log('ipfs.files.get -> ' + err));
+  });
+};
 
 function log(str) {
   console.log(str);
