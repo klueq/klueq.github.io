b0e1bafb84565e24b23c26469a1262506245c8ab
klueq <43359723+klueq@users.noreply.github.com>
2018-11-02 23:48:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function init() {
 function loadScript() {
   return new Promise((resolve, reject) => {
     let ver = location.search.slice(1) || 'latest';
-    let url = /\.js$/.test(ver) ? ver : `https://unpkg.com/ipfs@${ver}/dist/index.js`;
+    let url = ver[0] == '/' ? ver : `https://unpkg.com/ipfs@${ver}/dist/index.js`;
     log('script: ' + url);
     let script = document.createElement('script');
     script.src = url;
