52242e1e9a176fac797a1ad476769fee257d0641
klueq <43359723+klueq@users.noreply.github.com>
2018-11-03 14:04:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const basetime = Date.now();
 const config = {
   relay: {enabled: true, hop: {enabled: true}},
   Addresses: {Swarm: ['/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star']},
+  EXPERIMENTAL: {dht: true},
 };
 
 const fcids = [
